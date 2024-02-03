f9974bbbd2565aec76dab7f7be62df583e6ff6b0
JesusG <py3user22@github.com>
2024-02-03 02:26:18 -0600

    203 save all the way up to menu

--- b_app203/urls.py
+++ b_app203/urls.py
@@ -4,6 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('home', views.home),
+    path('home', views.home, name="home"),
+    path('home2', views.home1),
+
+    #203 menu urls add.
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),
+
+    #203 header links add
+    path('about/', views.about, name="about"),
+
 
 ]
--- b_app203/views.py
+++ b_app203/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from .models import Menu
+
 
 # Create your views here.
-def home(request):
+def home1(request):
     return render(request, '0203_django_notes2.html', {})
 
+#0203 menu method add.
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def home(request):
+    return render(request, 'index.html',{})
+
+def display_menu_item(request, pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item = ""
+    return render(request, 'menu_item.html', {"menu_item": menu_item})
